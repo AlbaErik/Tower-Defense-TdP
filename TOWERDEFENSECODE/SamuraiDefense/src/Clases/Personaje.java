0faf22a94d531f9d7c4ea5ddbe7939ce66d4af17
NahuelMaika <nahuelmaika@gmail.com>
2019-09-08 16:36:35 -0300

    Agregue clase Entidad e hijos
    Clase Entidad,Personaje,Defensor y EspadachinElite(solo implementacion)

---
+++ TOWERDEFENSECODE/SamuraiDefense/src/Clases/Personaje.java
@@ -0,0 +1,20 @@
+package Clases;
+
+
+public abstract class Personaje extends Entidad {
+	protected int Damage;
+	protected int Range;
+	protected int AttackSpeed;
+	//protected Arma Weapon;
+	
+	public Personaje(int x, int y) {
+		super(x, y);
+	}
+
+		
+	public abstract int getDamage();
+	
+	public abstract int getRange();
+	
+	public abstract int getAttackSpeed();
+}
