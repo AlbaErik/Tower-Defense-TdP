83cd7c0a342bf60aaa78a1f36167d6c241ea140d
NahuelMaika <nahuelmaika@gmail.com>
2019-10-16 10:27:44 -0300

    PNG REPOSO  ATACANTES
    Mas tarde hay que cambiarlas por gifs

--- TOWERDEFENSECODE/SamuraiDefense/src/Grafica/Entidades/Atacantes/NecromanteGrafico.java
+++ TOWERDEFENSECODE/SamuraiDefense/src/Grafica/Entidades/Atacantes/NecromanteGrafico.java
@@ -10,11 +10,12 @@ public class NecromanteGrafico extends AtacanteGrafico{
 
 	public NecromanteGrafico(int x, int y, PanelMapa m, Entidad e) {
 		super(x, y, m, e);
-		graficos = new ImageIcon[3];
+		graficos = new ImageIcon[4];
 
 		for (int i = 0; i < palabras.length; i++) {
 			graficos[i] = new ImageIcon("Sprites/Personajes/Atacantes/" + palabras[i] + ".gif");
 		}
+		graficos[3]=new ImageIcon("Sprites/Personajes/Atacantes/NecromanteStanding.png");
 		setGraficoInicio(x, y);
 	}
 
